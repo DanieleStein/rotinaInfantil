6e1fc4344c7a5d953ef57a5cbb8d03cd3c043ba2
Daniele Stein <danieleastein@gmail.com>
2023-09-22 16:07:01 -0300

    Recendo as informacoes do radio group

--- app/src/main/java/br/com/rotinainfantil/ConfirmationFragment.kt
+++ app/src/main/java/br/com/rotinainfantil/ConfirmationFragment.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -34,7 +38,11 @@ class ConfirmationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirmation, container, false)
+        val view = inflater.inflate(R.layout.fragment_confirmation, container, false)
+        val args: ConfirmationFragmentArgs by navArgs()
+      view.findViewById<TextView>(R.id.confirmationTextView).text = "${args.stack}"
+
+      return view
     }
 
     companion object {
